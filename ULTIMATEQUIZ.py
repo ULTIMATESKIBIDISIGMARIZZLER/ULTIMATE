25df9c150aba5e19963e3aa978181b1bf803dbf6
ULTIMATESKIBIDISIGMARIZZLER <yjyj0209yjyj@gmail.com>
2024-09-15 20:57:50 +0200

    Update ULTIMATEQUIZ.py

--- ULTIMATEQUIZ.py
+++ ULTIMATEQUIZ.py
@@ -12,11 +12,11 @@ answer_box2=Rect(0,0,300,150)
 answer_box3=Rect(0,0,300,150)
 answer_box4=Rect(0,0,300,150)
 
-skip_box=(0,0,150,330)
+skip_box=Rect(0,0,150,330)
 score=0
 time_left=30
 
-question_file_name="ULTIMATEQUIZ/questions.txt"
+question_file_name="questions.txt"
 marquee_message=""
 is_game_over=False
 answer_boxes=[answer_box1,answer_box2,answer_box3,answer_box4]
@@ -34,7 +34,7 @@ answer_box3.move_ip(20,450)
 answer_box4.move_ip(370,450)
 skip_box.move_ip(700,270)
 
-def draw():
+def draw(): #making rectangles
     global marquee_message
     screen.clear()
     screen.fill(color="black")
@@ -43,94 +43,103 @@ def draw():
     screen.draw.filled_rect(skip_box,"green")
     screen.draw.filled_rect(timer_box,"navy blue")
 
-for answer_box in answer_boxes:
-    screen.draw.filled_rect(answer_box,"dark orange")
-
-marquee_message="Welcome to the ULTIMATE QUIZ......"
-
-marquee_message=marquee_message+f"Q:{question_index} of {question_count}"
-
-screen.draw.textbox(marquee_message, marquee_box, color="white")
-
-screen.draw.textbox(
-    str(time_left),timer_box,
-    color="white", shadow=(0.5,0.5),
-    scolor="dim grey"
-)
-
-screen.draw.textbox(
-    "Skip",skip_box,
-    color="black", angle=-90
-)
-
-screen.draw.textbox(
-    question[0].strip(), questions_box,
-    color="white", shadow=(0.5,0.5),
-    scolor="dim grey"
-)
-
-index=1
-
-for anwer_box in answer_boxes:
-    screen.draw.textbox(question[index].strip(),answer_box, color="black")
-    index=index+1
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    for answer_box in answer_boxes:
+        screen.draw.filled_rect(answer_box,"dark orange")
+
+    marquee_message="Welcome to the ULTIMATE QUIZ......"
+
+    marquee_message=marquee_message+f"Q:{question_index} of {question_count}"
+
+    screen.draw.textbox(marquee_message, marquee_box, color="white")
+
+    screen.draw.textbox(
+        str(time_left),timer_box,
+        color="white", shadow=(0.5,0.5),
+        scolor="dim grey"
+    )
+
+    screen.draw.textbox(
+        "Skip",skip_box,
+        color="black", angle=-90
+    )
+
+    screen.draw.textbox(question[0].strip(), questions_box,
+        color="white", shadow=(0.5,0.5),
+        scolor="dim grey"
+    )
+
+    index=1
+
+    for answer_box in answer_boxes:
+        screen.draw.textbox(question[index].strip(),answer_box, color="black")
+        index=index+1
+
+def update():
+    move_marquee()       #moving marqueebox
+
+def move_marquee():
+    marquee_box.x=marquee_box.x-2
+    if marquee_box.right < 0:
+        marquee_box.left=WIDTH
+
+def reading():
+    global question_count, questions
+    q_file=open(question_file_name,"r")
+    for question in q_file:
+        questions.append(question)
+        question_count=question_count+1
+    q_file.close()                      #it opens question file.
+
+def read_next_question():
+    global question_index
+    question_index=question_index+1
+    return questions.pop(0).split(",")     #it will give you next question.
+
+def on_mouse_down(pos):
+    index=1
+    for box in answer_boxes:
+        if box.collidepoint(pos):
+            if index is int(question[5]):
+                correct_answer()
+            else:
+                game_over()
+        index=index+1
+    if skip_box.collidepoint(pos):
+        skip_question()                    #it will give you a sign if you click the answer_boxes.
+
+def correct_answer():
+    global score, question, time_left, questions
+    score=score+1
+    if questions:
+        question=read_next_question()
+        time_left=30
+    else:
+        game_over()
+    
+def game_over():
+    global question, time_left, is_game_over
+    message=f"game_over \n You got{score} question correct"
+    question=[message,"-","-","-","-",5]
+    time_left=0
+    is_game_over=False
+
+def skip_question():
+    global question, time_left
+    if questions and not is_game_over: #if you got more question but the game is not over
+        question=read_next_question()
+        time_left=30
+    else:
+        game_over()
+
+def update_time_left():
+    global time_left
+    if time_left: #if there is more time then time has go down
+        time_left=time_left-1
+    else:
+        game_over
+
+reading()
+question=read_next_question()
+clock.schedule_interval(update_time_left,1)
+
+pgzrun.go()
